64633ed530e111a64ae254b180ab63eed9dd9d0b
Katharine Cooney <katharinecooney@gmail.com>
2019-06-29 15:53:10 -0500

    Add regular, interpolated, and styled console logs

--- app.js
+++ app.js
@@ -7,10 +7,14 @@ const dogs = [{ name: 'Snickers', age: 2 }, { name: 'hugo', age: 8 }];
     }
 
     // Regular
+    console.log('Hello! I am just a regular console log!')
 
     // Interpolated
+    console.log('Hello! I can %s! How cool!', 'interpolate')
 
     // Styled
+    // whatever text follows the %c will be styled
+    console.log('%c I am large, pink text with a purple background!', 'font-size: 30px; color: pink; background-color: purple')
 
     // warning!
 
